ab8a3fedab172274129afbaf60173d69f64bd23c
luisanoosa <luisazovico@icloud.com>
2026-09-10 13:31:50 +0200

    Update test output messages to uppercase

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ if __name__ == '__main__':
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 def in_autotests_we_trust(a, b):
     if a == b:
-        print('Passou no teste')
+        print('PASS')
     else:
-        print('Falhou no teste')
+        print('FALL')
 
 in_autotests_we_trust(10, '10')
 
